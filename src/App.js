a05e7961f5e5b2eb1c108d299e45deaad8798b39
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2022-02-13 11:52:49 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import About from "./Components/About";
+import Alert from "./Components/Alert";
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const [alert, setalert] = useState(null); //We'll use alert variable as an object
+
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert('Dark mode enabled!', 'success');
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light mode enabled!', 'success');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar title="Text-Utils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-4">
+        <switch>
+          <Route exact path="/">
+            <TextForm showAlert={showAlert} heading="Enter text below" mode={mode} />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </switch>
+      </div>
+    </Router>
+  );  
 }
 
 export default App;
